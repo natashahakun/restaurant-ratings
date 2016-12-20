44fb49362f68f967ef999d3ebf0f7f8f8563466c
Natasha Hakun <hakun.natasha@gmail.com>
2016-12-20 15:16:39 -0500

    Add search bar to dashboard.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 angular.module("myApp").controller('DashboardController', ['$scope', '$rootScope', 'RestaurantsService', 'RestaurantDetailService', function($scope, $rootScope, RestaurantsService, RestaurantDetailService) {
 
 	$scope.restaurants = RestaurantsService.restaurants;
+	$scope.restaurantSearch = '';
 
 	$scope.showRestaurantInfo = function(restaurant) {
 		if (restaurant !== $scope.selectedRestaurant) {
